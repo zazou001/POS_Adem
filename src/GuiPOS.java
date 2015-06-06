2c7380fbefa95b087d355514a6f885552abb8acb
Yt-trium <cyril.meyer.68@gmail.com>
2015-06-06 02:24:46 +0200

    GUI Inscription

--- src/GuiPOS.java
+++ src/GuiPOS.java
@@ -32,9 +32,11 @@ public class GuiPOS extends JFrame
 	private static final long serialVersionUID = 42L;
 	
 	private int grp = 0;
+	private POS POSfunc;
 	
     GuiPOS(String user, String pass, int grp)
     {
+		POSfunc = new POS();
 		this.grp = grp;
 		initGUI();
 		System.out.println("- LOGIN -");
@@ -44,7 +46,40 @@ public class GuiPOS extends JFrame
     {
 		JTabbedPane tabbedPane = new JTabbedPane();
 		ImageIcon noIcon = new ImageIcon("img/no.gif", "NO_ICON");
+		
 		final JPanel panel1 = new JPanel();
+        panel1.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+        panel1.setLayout(new GridLayout(16,1));
+		
+		// ------------------------- PANEL 1 -------------------------
+		final JLabel panel1label1 = new JLabel("Nom :");
+		final JTextField panel1nom = new JTextField(42);
+		final JLabel panel1label2 = new JLabel("Prenom :");
+		final JTextField panel1prenom = new JTextField(42);
+		final JLabel panel1label3 = new JLabel("Année D’études :");
+		String[] panel1anneeStrings = { "L1", "L2MATH", "L3MATH", "L2INFO", "L3INFO", "AUTRES"};
+		final JComboBox<String> panel1annee = new JComboBox<String>(panel1anneeStrings);
+		final JLabel panel1label4 = new JLabel("");
+		final JButton panel1valider = new JButton("Valider");
+		panel1.add(panel1label1);
+		panel1.add(panel1nom);
+		panel1.add(panel1label2);
+		panel1.add(panel1prenom);
+		panel1.add(panel1label3);
+		panel1.add(panel1annee);
+		panel1.add(panel1label4);
+		panel1.add(panel1valider);
+		// Bouton de validation
+		panel1valider.addActionListener(new ActionListener()
+        {
+            //@Override
+            public void actionPerformed(ActionEvent event)
+            {
+				POSfunc.ajoutMembre(panel1nom.getText(), panel1prenom.getText(), panel1annee.getSelectedItem().toString());
+            }
+        });
+		
+		// ------------------------- PANEL 2 -------------------------
 		final JPanel panel2 = new JPanel();
 		final JPanel panel3 = new JPanel();
 		final JPanel panel4 = new JPanel();
--- src/POS.java
+++ src/POS.java
@@ -10,3 +10,22 @@
  * Synopsis :
  * 	Fonctions Spécifique du POS
  * ---------- ---------- ---------- */
+
+import javax.swing.*;
+
+public class POS
+{
+	POS()
+	{
+		
+	}
+	
+	/*
+	 * Permet l'ajout de membres dans la base de donnée
+	 */
+	public int ajoutMembre(String nom, String prenom, String annee)
+	{
+		JOptionPane.showMessageDialog(null, "Ajout du membre " + nom + " " + prenom + " etudiant en " + annee);
+		return 0;
+	}
+}
